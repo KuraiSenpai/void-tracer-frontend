35275e44b4638c115a2c5844b1c8362d1dba61e0
Yoni Cant <yoni.cant@student.kdg.be>
2026-03-03 13:04:58 +0100

    Remove unused imports

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { AlertList } from './features/alerts/alert-list';
-import { Calendar1999 } from './features/calendar/calendar';
-import { FissureList } from './features/fissures/fissure-list';
-import { InvasionList } from './features/invasions/invasion-list';
-import { VendorList } from './features/vendors/vendor-list';
-import { Header } from "./shared/components/header/header";
-import { RouterModule } from "@angular/router";
+import { RouterModule } from '@angular/router';
+import { Header } from './shared/components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [AlertList, FissureList, VendorList, InvasionList, Calendar1999, Header, RouterModule],
+  imports: [Header, RouterModule],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
--- src/app/features/world-state/world-state.ts
+++ src/app/features/world-state/world-state.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { Calendar1999 } from "../calendar/calendar";
-import { VendorList } from "../vendors/vendor-list";
-import { AlertList } from "../alerts/alert-list";
-import { FissureList } from "../fissures/fissure-list";
-import { InvasionList } from "../invasions/invasion-list";
+import { AlertList } from '../alerts/alert-list';
+import { Calendar1999 } from '../calendar/calendar';
+import { FissureList } from '../fissures/fissure-list';
+import { InvasionList } from '../invasions/invasion-list';
+import { VendorList } from '../vendors/vendor-list';
 
 @Component({
   selector: 'app-world-state',
@@ -11,6 +11,4 @@ import { InvasionList } from "../invasions/invasion-list";
   templateUrl: './world-state.html',
   styleUrl: './world-state.scss',
 })
-export class WorldState {
-
-}
+export class WorldState {}
