7d9ccec7ffa704ee0c35f3bd084c61cb32fa0c1a
Yoni Cant <yonicant@live.be>
2026-03-05 19:57:21 +0100

    Angular logic for dynamic column system on multiple screen sizes

--- src/app/features/world-state/world-state.ts
+++ src/app/features/world-state/world-state.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { AlertList } from '../alerts/alert-list';
 import { Calendar1999 } from '../calendar/calendar';
 import { FissureList } from '../fissures/fissure-list';
@@ -11,4 +11,31 @@ import { VendorList } from '../vendors/vendor-list';
   templateUrl: './world-state.html',
   styleUrl: './world-state.scss',
 })
-export class WorldState {}
+export class WorldState {
+  private widgets = ['alerts', 'vendors', 'calendar', 'invasions', 'fissures'];
+  private columnCount = 3;
+
+  @HostListener('window:resize')
+  onResize() {
+    this.setColumnCount();
+  }
+
+  ngOnInit() {
+    this.setColumnCount();
+  }
+
+  setColumnCount() {
+    const width = window.innerWidth;
+    if (width <= 768) this.columnCount = 1;
+    else if (width <= 1400) this.columnCount = 2;
+    else this.columnCount = 3;
+  }
+
+  getColWidgets(colIdx: number) {
+    return this.widgets.filter((_, i) => i % this.columnCount === colIdx);
+  }
+
+  get columnIndices() {
+    return Array.from({ length: this.columnCount }, (_, i) => i);
+  }
+}
