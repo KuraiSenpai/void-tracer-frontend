3650821a81ac4c9594235df06c49b8c834a99d09
Yoni Cant <yoni.cant@student.kdg.be>
2026-03-05 16:05:54 +0100

    env for prod

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,4 @@
 export const env = {
     production: false,
-    apiUrl: 'http://localhost:8080/api',
-    worldStateApiUrl: 'https://api.warframe.com/cdn/worldState.php'
+    apiUrl: 'http://localhost:8080/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const env = {
-    production: false,
-    apiUrl: 'http://localhost:8080/api'
+  production: true,
+  apiUrl: 'https://secondary-cockroach-kuraisenpai-32f3895d.koyeb.app/api',
 };
